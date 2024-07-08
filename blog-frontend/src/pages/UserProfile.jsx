75b3ecc7c7f6ee468ad55ff2e039957e85f7c2c0
Manii1997 <manibct1817@gmail.com>
2024-07-08 10:02:07 +0530

    user profile update

--- blog-frontend/src/pages/UserProfile.jsx
+++ blog-frontend/src/pages/UserProfile.jsx
@@ -2,8 +2,22 @@ import React from "react";
 
 const UserProfile = () => {
   return (
-    <div>
-      <h1 className="container mx-auto mt-20 p-5 font-bold">Profile</h1>
+    <div className="container mx-auto mt-20 p-5">
+      <h1 className="font-bold">Profile</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
+        dolore impedit veritatis cum deserunt non voluptates, et commodi nostrum
+        doloribus quasi illo delectus amet sapiente reprehenderit vel,
+        aspernatur accusamus. Sint tenetur dignissimos dolores accusantium rerum
+        saepe, dolorum blanditiis veniam. Maxime sequi harum fuga, corporis
+        aperiam quibusdam deserunt libero minus qui illum dolorem nesciunt
+        aliquam? Illum temporibus, aliquam veritatis distinctio perspiciatis
+        blanditiis maxime, beatae quaerat necessitatibus accusantium unde
+        provident voluptas, odio earum dignissimos veniam. Blanditiis sint
+        repellat maxime dolorem delectus fugiat, vitae molestias facilis
+        possimus? Dignissimos ab delectus, magni minus fugit aspernatur sed
+        officiis assumenda, nobis atque iste adipisci. Facilis, temporibus.
+      </p>
     </div>
   );
 };
